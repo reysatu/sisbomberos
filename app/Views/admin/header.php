0ba340d3f8b97996a75e90afd5cf984fe0cf4d6f
jcColala <colalon25@egmail.com>
2020-09-01 19:15:07 -0500

    subiendo alerta

--- app/Views/admin/header.php
+++ app/Views/admin/header.php
@@ -18,5 +18,10 @@
   <!-- Select2 -->
   <link rel="stylesheet" href="<?php echo base_url();?>/public/admin/plugins/select2/css/select2.min.css">
   <link rel="stylesheet" href="<?php echo base_url();?>/public/admin/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
+
+  <!-- ----------------------------------------------------alertas---------------------------------------------------- -->
+    <link rel="stylesheet" href="<?php echo base_url()?>assets/alertifyJS/css/themes/semantic.min.css">
+    <link rel="stylesheet" href="<?php echo base_url()?>assets/alertifyJS/css/alertify.min.css">
+    <script  src="<?php echo base_url()?>assets/alertifyJS/alertify.min.js"></script>
 </head>
 <body class="hold-transition sidebar-mini">
