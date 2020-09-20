dddd48df899b034f9cf8eda3a1a7702b636db48c
reysan <reysangama7@gmail.com>
2020-09-20 08:49:44 +0200

    cmabios1

--- app/Views/intranet/content.php
+++ app/Views/intranet/content.php
@@ -19,7 +19,7 @@
           <div class="col-lg-12">
             <div class="card">
               <div class="card-body">
-                <table id="example1" class="table table-bordered table-striped">
+                <table  class="table table-bordered table-striped">
                 <thead>
                 
                 </thead>
